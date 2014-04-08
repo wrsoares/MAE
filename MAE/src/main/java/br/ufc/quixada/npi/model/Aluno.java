b0f0cb13846416926d0ff7d80fdbcfe7ef75a05f
William <William@200.129.38.163>
2014-04-08 15:04:09 -0300

    Configurações básicas do projeto

---
+++ MAE/src/main/java/br/ufc/quixada/npi/model/Aluno.java
@@ -0,0 +1,38 @@
+package br.ufc.quixada.npi.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Aluno {
+	
+	private String nome;
+	private String matricula;
+	private String email;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
